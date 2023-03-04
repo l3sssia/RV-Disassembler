2ac013abd84bc61a90bebfff69f80a09eb535a08
l3sssia <103117355+l3sssia@users.noreply.github.com>
2023-03-04 21:38:18 +0300

    fix

--- RVDisassembler.java
+++ RVDisassembler.java
@@ -2,6 +2,9 @@ import elf.ElfFile;
 
 public class RVDisassembler {
     public static void main(String[] args) {
+        if (args.length < 2) {
+            System.out.println("Enter 2 arguments: input file name (elf) and output file name");
+        }
         ElfFile elf = new ElfFile(args[0]);
         elf.parse();
         elf.write(args[1]);
